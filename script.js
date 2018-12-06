bd1d470d614a76ee6e4fb275f7f0a3c3750178c7
Dawid <dawid.debo2.pl@gmail.com>
2018-12-06 19:49:21 +0100

    added getRandomKey and addClass

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function losuj(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min);
 
-}
-console.log(losuj(2, 5))
+};
+
+
+
+
+const buttonsKey = document.getElementsByClassName("key");
+let lastKey;
+
+
+function getRandomKey() {
+  index = Math.floor(Math.random() * buttonsKey.length);
+  const key = buttonsKey[index];
+  if (key === lastKey) {
+      console.log("The same key")
+      return getRandomKey()
+    }
+    lastKey = key;
+    return key;
+};
+console.log(getRandomKey());
+function addClass() {
+    getRandomKey().className="fire-key";
+};
+addClass();
+
+
+
+    /* do domu stworz funkcje ktora losowo dodaje do klawisze klase fire-key  */
+
