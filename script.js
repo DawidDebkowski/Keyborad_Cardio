001414117eb03e147861b103c3e2edb560aacb3b
Andrzej <deceeededced>
2018-12-13 18:17:47 +0100

    up

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-function losuj(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-
-};
-
-
-
-
-const buttonsKey = document.getElementsByClassName("key");
+const buttonsKey = document.getElementsByClassName('key');
 let lastKey;
 
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 function getRandomKey() {
-  index = Math.floor(Math.random() * buttonsKey.length);
-  const key = buttonsKey[index];
-  if (key === lastKey) {
-      console.log("The same key")
-      return getRandomKey()
+    index = Math.floor(Math.random() * buttonsKey.length);
+    const key = buttonsKey[index];
+    if (key === lastKey) {
+        console.log('The same key!!!')
+        return getRandomKey()
     }
     lastKey = key;
     return key;
 };
-console.log(getRandomKey());
-function addClass() {
-    getRandomKey().className="fire-key";
-};
-addClass();
+
+
+console.log(getRandomKey())
+
+// do domu napisz funkcję która losowo dodaje do klawisza klasę fire-key
 
 
 
-    /* do domu stworz funkcje ktora losowo dodaje do klawisze klase fire-key  */
 
